1a66a27ba7f588685856dc1403a39c67f15bece8
Jenna <jenna.rantanen@helsinki.fi>
2020-08-22 22:31:35 +0300

    Poistettu muistiinpanot

--- app.py
+++ app.py
@@ -5,13 +5,3 @@ app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
 import routes
-
-# TODO: lisää groups.db rajoitukset ja aihe?
-# TODO: omat chätit: osa viimeisintä viestiä ja sen kellon aika (tai pvämäärä)
-# TODO: uudet chätit: nimi ja kuvaus ja osallistujien määrä n/x
-# TODO: mahdollisuus piilottaa chatit?
-# TODO: search toimii ilman napin painallusta
-
-#todo: scroll chattiin
-
-#TODO: eriväriset viestit riippuen lägettäjästä
