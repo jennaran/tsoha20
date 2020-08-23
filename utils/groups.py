2f7ff1a4d8e136824c33647d4e3ab70c8712acff
Jenna <jenna.rantanen@helsinki.fi>
2020-08-23 22:23:47 +0300

    Kirjaimien koolla ei nyt ole väliä chattejä haettaessa

--- utils/groups.py
+++ utils/groups.py
@@ -19,7 +19,7 @@ def get_filtered_groups(filter):
                 "SELECT G.id " \
                 "FROM groups G, user_groups UG, users U " \
                 "WHERE U.id = UG.user_id AND UG.group_id = G.id AND U.id = :id) " \
-          "AND (G.name LIKE :name OR T.name LIKE :name)"
+          "AND (LOWER(G.name) LIKE :name OR LOWER(T.name) LIKE :name) AND G.is_full = false"
     result = db.session.execute(sql, {"id": users.user_id(), "name": "%"+filter+"%"})
     return result.fetchall()
 
@@ -38,6 +38,12 @@ def get_name(group_id):
     return result.fetchall()
 
 
+def get_max_members(group_id):
+    sql = "SELECT DISTINCT max_members from groups WHERE id = :group_id"
+    result = db.session.execute(sql, {"group_id": group_id})
+    return result.fetchone()
+
+
 def get_member_count(group_id):
     sql = "SELECT COUNT(*) FROM user_groups WHERE group_id = :group_id"
     result = db.session.execute(sql, {"group_id": group_id})
@@ -47,7 +53,8 @@ def get_member_count(group_id):
 def get_members(group_id):
     sql = "SELECT DISTINCT U.username " \
           "FROM groups G, user_groups UG, users U " \
-          "WHERE U.id = UG.user_id AND UG.group_id = G.id AND G.id = :group_id"
+          "WHERE U.id = UG.user_id AND UG.group_id = G.id AND G.id = :group_id " \
+          "ORDER BY U.username"
     result = db.session.execute(sql, {"group_id": group_id})
     return result.fetchall()
 
@@ -58,20 +65,49 @@ def is_a_member(group_id):
     return result.fetchall()
 
 
-def join_a_group(group_id):
+def is_full(group_id):
+    print("full")
+    sql = "SELECT is_full FROM groups WHERE id=:group_id"
+    result = db.session.execute(sql, {"group_id": group_id})
+    print(result.fetchone())
+    return result.fetchone()
+
+
+def set_full(group_id):
     try:
-        sql = "INSERT INTO user_groups (user_id, group_id) VALUES (:user_id, :group_id)"
-        db.session.execute(sql, {"user_id": users.user_id(), "group_id": group_id})
+        sql = "UPDATE groups SET is_full = true WHERE id = :group_id"
+        db.session.execute(sql, {"group_id": group_id})
         db.session.commit()
     except:
         return False
     return True
 
 
+def join_a_group(group_id):
+    if is_full(group_id):
+        return False
+    else:
+       print("ryhmässä tilaa")
+       try:
+           sql = "INSERT INTO user_groups (user_id, group_id) VALUES (:user_id, :group_id)"
+           db.session.execute(sql, {"user_id": users.user_id(), "group_id": group_id})
+           member_count = get_member_count(group_id)
+           max_members = get_max_members(group_id)
+           if member_count == max_members:
+               set_full(group_id)
+           db.session.commit()
+       except:
+           return False
+       return True
+
+
+
 def leave_a_group(group_id):
     try:
         sql = "DELETE FROM user_groups WHERE user_id = :user_id AND group_id = :group_id"
         db.session.execute(sql, {"user_id": users.user_id(), "group_id": group_id})
+        sql2 = "UPDATE groups SET is_full = false WHERE id = :group_id"
+        db.session.execute(sql2, {"group_id": group_id})
         db.session.commit()
     except:
         return False
